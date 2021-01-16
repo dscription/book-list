a374823f64c03c825c11ae0f2c96d40c90be6ed7
dscription <dscriptionplayground@gmail.com>
2021-01-16 12:55:36 -0500

    add lodash library and create resolver function

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,15 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 // Define the schema, the object types and relations between object types and how you can reach into the graph and interact with that data, query or mutate the data.
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+var books = [
+  { name: 'Eloquent JavaScript', genre: 'Computer Science', id: '1' },
+  { name: 'Introduction To Graph Theory', genre: 'Computer Science', id: '2' },
+  { name: 'Cracking the Coding Interview', genre: 'Computer Science', id: '3' },
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -21,13 +29,14 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       // Define which arguments should be expected when the query is made (ie.book id)
       args: { id: { type: GraphQLString } },
-      resolve(parent, args){
+      resolve(parent, args) {
         // code to get data from db / other source
-      }
+        return _.find(books, { id: args.id });
+      },
     },
   },
 });
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
-})
+  query: RootQuery,
+});
