1b17323208bb6dd859a3e8c9d35368799937eecb
dscription <dscriptionplayground@gmail.com>
2021-01-16 13:25:53 -0500

    add GraphQLID type

--- server/app.js
+++ server/app.js
@@ -6,7 +6,8 @@ const app = express();
 
 app.use('/graphql', graphqlHTTP({
   // schema: schema , can be shortened using es6 duplicate property name feature.
-  schema
+  schema,
+  graphiql: true
 }))
 
 app.listen(4000, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,7 +2,7 @@ const graphql = require('graphql');
 const _ = require('lodash');
 
 // Define the schema, the object types and relations between object types and how you can reach into the graph and interact with that data, query or mutate the data.
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
 // dummy data
 var books = [
@@ -14,8 +14,8 @@ var books = [
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
-    name: { tyle: GraphQLString },
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
 });
@@ -28,7 +28,7 @@ const RootQuery = new GraphQLObjectType({
     book: {
       type: BookType,
       // Define which arguments should be expected when the query is made (ie.book id)
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db / other source
         return _.find(books, { id: args.id });
