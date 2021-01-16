da6ef0175aeca871904f3a489f0d7122a1c100c1
dscription <dscriptionplayground@gmail.com>
2021-01-16 13:29:40 -0500

    create AuthorType

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,7 +2,13 @@ const graphql = require('graphql');
 const _ = require('lodash');
 
 // Define the schema, the object types and relations between object types and how you can reach into the graph and interact with that data, query or mutate the data.
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = graphql;
 
 // dummy data
 var books = [
@@ -11,12 +17,27 @@ var books = [
   { name: 'Cracking the Coding Interview', genre: 'Computer Science', id: '3' },
 ];
 
+var authors = [
+  { name: 'Marijn Haverbeke', age: 50, id: '1' },
+  { name: 'Richard J. Trudeau', age: 40, id: '2' },
+  { name: 'Gayle McDowell', age: 35, id: '3' },
+];
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
