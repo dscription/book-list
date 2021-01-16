a41c002c4e00a9cb98a6e729af58eb6db0381aa7
dscription <dscriptionplayground@gmail.com>
2021-01-16 12:28:58 -0500

    import schema into express graphql endpoint

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-  
+  // schema: schema , can be shortened using es6 duplicate property name feature.
+  schema
 }))
 
 app.listen(4000, () => {
