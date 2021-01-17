5ffbe7b9a82d63a5e1986a6e31c1c5ead7a7cfde
dscription <dscriptionplayground@gmail.com>
2021-01-17 13:31:25 -0500

    remove dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,34 +11,6 @@ const {
   GraphQLList,
 } = graphql;
 
-// dummy data
-var books = [
-  {
-    name: 'Eloquent JavaScript',
-    genre: 'Computer Science',
-    id: '1',
-    authorId: '1',
-  },
-  {
-    name: 'Introduction To Graph Theory',
-    genre: 'Computer Science',
-    id: '2',
-    authorId: '2',
-  },
-  {
-    name: 'Cracking the Coding Interview',
-    genre: 'Computer Science',
-    id: '3',
-    authorId: '3',
-  },
-];
-
-var authors = [
-  { name: 'Marijn Haverbeke', age: 50, id: '1' },
-  { name: 'Richard J. Trudeau', age: 40, id: '2' },
-  { name: 'Gayle McDowell', age: 35, id: '3' },
-];
-
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
@@ -50,7 +22,7 @@ const BookType = new GraphQLObjectType({
       // The parent data would be the Book that we already found. The data is passed to the resolve function.
       resolve(parent, args) {
         console.log(parent);
-        return _.find(authors, { id: parent.authorId });
+        // return _.find(authors, { id: parent.authorId });
       },
     },
   }),
@@ -65,7 +37,7 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return _.filter(books, { authorId: parent.id });
+        // return _.filter(books, { authorId: parent.id });
       },
     },
   }),
@@ -82,14 +54,14 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db / other source
-        return _.find(books, { id: args.id });
+        // return _.find(books, { id: args.id });
       },
     },
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(authors, { id: args.id });
+        // return _.find(authors, { id: args.id });
       },
     },
     books: {
