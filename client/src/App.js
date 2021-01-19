877982125541bd526875aeefa33a8b3e192d4d93
dscription <dscriptionplayground@gmail.com>
2021-01-19 16:56:04 -0500

    add book component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import BookList from './components/BookList';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import AddBook from './components/AddBook';
 
 // apollo client setup
 const client = new ApolloClient({
@@ -13,6 +14,7 @@ function App() {
       <div className="App">
         <h1>My Reading List</h1>
         <BookList />
+        <AddBook />
       </div>
     </ApolloProvider>
   );
